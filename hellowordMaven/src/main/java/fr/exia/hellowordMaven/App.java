31070e3b2ff36a553815d6006cc8dcee12803932
faridrr <farid.f1@hotmail.fr>
2018-05-22 09:44:48 +0100

    HelloWordMaven
    22/05/2018

---
+++ hellowordMaven/src/main/java/fr/exia/hellowordMaven/App.java
@@ -0,0 +1,13 @@
+package fr.exia.hellowordMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
